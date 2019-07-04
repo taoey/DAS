499c524299b35b5ce4b7699bb75e3ca827694de9
Taoey <741494582@qq.com>
2019-07-04 15:27:31 +0800

    读取图片信息api

---
+++ JavaBase/src/main/java/picdegree/PicDegree.java
@@ -0,0 +1,42 @@
+package picdegree;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+import java.io.File;
+
+public class PicDegree {
+
+    private static  String getPicInfo(String imagePath) {
+        File picFile= new File(imagePath);
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(picFile);
+
+            for (Directory directory : metadata.getDirectories()){
+                for (Tag tag : directory.getTags()){
+                    if(tag.getTagName().equals("Orientation")){
+                        System.out.println(tag.getDescription());
+                    }
+                }
+                if (directory.hasErrors()){
+                    for (String error : directory.getErrors()){
+                        System.err.println("ERROR: " + error);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String picpath = "E:\\projects\\back-end\\DAS\\JavaBase\\src\\main\\java\\picdegree\\pics\\IMG_20190704_150836.jpg";
+        String picInfo = getPicInfo(picpath);
+    }
+
+}
